f07ce9989fa71c26605db84302e0f21d16ebd9c9
v01dxyz <v01dxyz@v01d.xyz>
2026-01-29 22:20:01 +0100

    update

---
+++ tests/hollywood2/run_in_container.sh
@@ -0,0 +1,11 @@
+# /pkg contains the container package
+
+md5sum_pyx() {
+    cat $(find $1 -name "*.pyx" | sort) | md5sum | cut -d" " -f1
+}
+
+if [[ $(md5sum_pyx /pkg/src) != $(md5sum_pyx /src/src) ]]; then
+    pip install /src/vision_toolkit_c
+fi
+
+pip install "/src[test]"
