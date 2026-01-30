317c4fcae4a5c538208213e01fa39e9d0f416497
v01dxyz <v01dxyz@v01d.xyz>
2026-01-30 09:19:40 +0100

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,57 @@
-from setuptools import Extension, setup, find_packages
+import os
+import pathlib
 import numpy as np
-from Cython.Build import cythonize 
 
-setup(
-     name="vision_toolkit",
-     version="0.1",
-     packages=find_packages(where="src"),
-     package_dir={"": "src"}, 
-)
+from setuptools import Extension, setup, find_packages
+from Cython.Build import cythonize
 
+SRC_DIR = pathlib.Path("src")
+
+def get_cython_pkgs_and_ext_modules():
+    ext_modules = []
+    pkgs = []
+
+    for p in (SRC_DIR / "vision_toolkit").glob("**/*.pyx"):
+        pkg = ".".join(p.parts[1:-1])
+
+        ext_module = Extension(
+            pkg,
+            sources=[p],
+        )
+
+        pkgs.append(pkg)
+        ext_modules.append(ext_module)
+
+        return pkgs, ext_modules
+
+cython_pkgs, cython_ext_modules = get_cython_pkgs_and_ext_modules()
+
+VISION_TOOLKIT_BUILD = os.getenv("VISION_TOOLKIT_BUILD", "all").lower()
+
+kwargs = {
+    "version": "0.1",
+    "package_dir": {"": "src"},
+}
+
+c_kwargs = {
+    "packages": cython_pkgs,
+    "ext_modules": cythonize(cython_ext_modules, language_level="3"),
+    "include_dirs": [np.get_include()],
+}
+py_kwargs = {
+    "packages": find_packages(where="src"),
+}
+
+# The following is a hack to allow separately building and packaging
+# the Cython package and the Python package.
+# It is meant to reduce CI time.
+if VISION_TOOLKIT_BUILD == "c":
+    kwargs.update(c_kwargs)
+    kwargs["name"] = "vision_toolkit_c"
+else:
+    if VISION_TOOLKIT_BUILD != "py":
+        kwargs.update(c_kwargs)
+    kwargs.update(py_kwargs)
+    kwargs["name"] = "vision_toolkit"
+
+setup(**kwargs)
