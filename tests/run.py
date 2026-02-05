912a8cf537665df86d8a0b927a47015b9cbf6dd7
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-05 05:46:13 +0100

    Convert tests to module

---
+++ tests/run.py
@@ -0,0 +1,28 @@
+import argparse
+import importlib
+import pathlib
+
+def get_cli_args():
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument("test_name")
+    arg_parser.add_argument("cutoff", type=int)
+    arg_parser.add_argument("report_name", nargs="?", default="report")
+
+    default_directory_root = "results"
+    arg_parser.add_argument("-d", "--directory", help=f"{default_directory_root}/<test_name>", type=pathlib.Path)
+
+    args = arg_parser.parse_args()
+
+    if args.directory is None:
+        args.directory = pathlib.Path(default_directory_root) / args.test_name
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_cli_args()
+
+    test_mod = importlib.import_module(f"{args.test_name}")
+
+    test_mod.EntryPoint.main(args)
