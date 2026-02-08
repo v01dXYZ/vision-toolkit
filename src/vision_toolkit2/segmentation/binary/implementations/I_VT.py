6f7c0f67f34bc92b4354a5e693b2799635c9865a
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-08 23:17:14 +0100

    Migrate segmentation utils, fix implementations

--- src/vision_toolkit2/segmentation/binary/implementations/I_VT.py
+++ src/vision_toolkit2/segmentation/binary/implementations/I_VT.py
@@ -1,90 +1,11 @@
 import numpy as np
-
-# from vision_toolkit.utils.segmentation_utils import (
-#     centroids_from_ints,
-#     interval_merging)
-
 import math
 
-from vision_toolkit2.oculomotor_series import AugmentedSerie
 from vision_toolkit2.config import Config
+from vision_toolkit2.segmentation.utils import interval_merging, centroids_from_ints
 
 from ..binary_segmentation_results import BinarySegmentationResults
 
-def centroids_from_ints(f_ints, x_coords, y_coords):
-    """
-
-    """
-    return [
-        comp_centroid(
-            x_coords[start : end + 1],
-            y_coords[start : end + 1],
-        )
-        for start, end in f_ints        
-    ]
-
-def comp_centroid(x_coords, y_coords):
-    """
-
-    """
-
-    return [x_coords.mean(), y_coords.mean()]
-
-def interval_merging(
-    x,
-    min_int_size=0,
-    max_int_size=np.inf,
-    p_o = 0,
-    s_o = 0,
-    status = None,
-    proportion = 0.95,
-):
-    """
-    This function takes a sequence of indexes and outputs a sequence of intervals corresponding to the indexes that are contiguous.
-
-    For instance, [0, 3, 4, 6, 8] outputs something like [(0, 0), (3, 6), (8, 8)].
-
-    Furthermore, it filters out intervals that are deemed not satisfying enough such as:
-
-    - not the right size: too small or too large
-    - status is a integrity vector, if an interval do not contain enough values with integrity it is rejected.    
-    """
-    is_break = (np.diff(x) >= 2).flatten()
-    idx_break, = is_break.nonzero()
-
-    # we build a 2 column array where the first column is the first
-    # index of the interval and the second is the last index of the
-    # interval
-    idx_intervals = np.zeros(
-        (len(idx_break) + 1, 2),
-        dtype=int,
-    )
-
-    # we initialize as the first interval starts at 0 and the last one
-    # ends at the last element
-    idx_intervals[0, 0] = 0
-    idx_intervals[-1, -1] = len(x) - 1
-
-    idx_intervals[:-1, 1] = idx_break
-    idx_intervals[1:, 0] = idx_break + 1
-
-    # Now we go back to the values stored in x
-    x_idx_intervals = np.take(x, idx_intervals)
-    x_size_intervals = x_idx_intervals[:, 1] - x_idx_intervals[:, 0] + s_o - p_o
-
-    # Now we filter out the intervals that are not deemed worthy
-    is_valid_interval = (min_int_size <= x_size_intervals)  & (x_size_intervals < max_int_size)
-
-    if status is not None:
-        status_proportion = np.array([
-            np.mean(status[start - p_o:end + s_o + 1]) 
-            for start, end in x_idx_intervals
-        ])
-        is_with_enough_status = status_proportion > proportion
-
-        is_valid_interval &= is_with_enough_status
-
-    return x_idx_intervals[is_valid_interval]
 
 def process_impl(s, config):
     idx_velocity_lower_than_threshold, = np.where(
@@ -151,21 +72,22 @@ def process_impl(s, config):
         i_lab[start : end + 1] = True
 
     return BinarySegmentationResults(
-        is_labeled= i_lab,
-        fixation_intervals= f_ints,
-        saccade_intervals= s_ints,
-        fixation_centroids= ctrds,
-        input = s,
-        config = config,
+        is_labeled=i_lab,
+        fixation_intervals=f_ints,
+        saccade_intervals=s_ints,
+        fixation_centroids=ctrds,
+        input=s,
+        config=config,
     )
 
-def default_config_impl(config, vf_diag):    
+
+def default_config_impl(config, vf_diag):
     if config.distance_type == "euclidean":
         v_t = vf_diag * 0.2
         return Config(
-            IVT_velocity_threshold = v_t,
+            IVT_velocity_threshold=v_t,
         )
     elif config.distance_type == "angular":
         return Config(
-            IVT_velocity_threshold = 50,
+            IVT_velocity_threshold=50,
         )
