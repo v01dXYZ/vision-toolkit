413e01bee8016721eaf47802133d33e8562d396b
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-09 13:36:05 +0100

    refactor: add version gateway pattern for dual v1/v2 testing

--- tests/run.py
+++ tests/run.py
@@ -5,17 +5,18 @@ import pathlib
 def get_cli_args():
     arg_parser = argparse.ArgumentParser()
 
+    arg_parser.add_argument("version", type=int, choices=[1, 2])
     arg_parser.add_argument("test_name")
     arg_parser.add_argument("cutoff", type=int)
     arg_parser.add_argument("report_name", nargs="?", default="report")
 
     default_directory_root = "results"
-    arg_parser.add_argument("-d", "--directory", help=f"{default_directory_root}/<test_name>", type=pathlib.Path)
+    arg_parser.add_argument("-d", "--directory", help=f"{default_directory_root}/<test_name>/<version>", type=pathlib.Path)
 
     args = arg_parser.parse_args()
 
     if args.directory is None:
-        args.directory = pathlib.Path(default_directory_root) / args.test_name
+        args.directory = pathlib.Path(default_directory_root) / args.test_name / f"v{args.version}"
 
     return args
 
