80590ab0a632a50d07360312c51bf1ea7d0fdb9f
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-09 16:41:43 +0100

    Fixing not waiting for all processes

--- tests/run_both_versions.sh
+++ tests/run_both_versions.sh
@@ -4,12 +4,8 @@ test_name="hollywood2"
 
 for version in 1 2; do
     ./run.py $version $test_name 3 &
-    pids_to_wait="$pid_to_wait $!"
 done
     
-wait $pids_to_wait
-echo "PWD:" $PWD
-ls -la
-echo results
-find results
+wait
+
 git diff --no-index ./results/$test_name/v{1,2}/report.md
