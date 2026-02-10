98d6a3d1f05eac518d329df940125a1b8a493c9c
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-10 13:30:22 +0100

    Try job in container (attemp 3)

--- tests/run_in_container.sh
+++ tests/run_in_container.sh
@@ -5,13 +5,14 @@ md5sum_pyx() {
     cat $(find $1 -name "*.pyx" | sort) | md5sum | cut -d" " -f1
 }
 
-
 VISION_TOOLKIT_BUILD="py"
 
-if [[ $(md5sum_pyx /pkg/src) != $(md5sum_pyx /src/src) ]]; then
+vision_toolkit_srcdir=$(dirname $0)/..
+
+if [[ $(md5sum_pyx /pkg/src) != $(md5sum_pyx $vision_toolkit_srcdir/src) ]]; then
     VISION_TOOLKIT_BUILD="all"
 fi
 
-VISION_TOOLKIT_BUILD=$VISION_TOOLKIT_BUILD pip install "/src[test]" --no-deps
+VISION_TOOLKIT_BUILD=$VISION_TOOLKIT_BUILD pip install "$vision_toolkit_srcdir/[test]" --no-deps
 
-cd /src/tests && $@
+$@
