e52d1823d9e8de2b1ca267008de45c55d0b6bbf4
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-11 16:59:54 +0100

    Simplify setup.py
    __init__.py were added to all ascendents of any pyx files as Cython
    uses it to autoname modules.

--- setup.py
+++ setup.py
@@ -18,41 +18,22 @@ def parse_boolean_env_var(key):
 VISION_TOOLKIT_COVERAGE = parse_boolean_env_var("VISION_TOOLKIT_COVERAGE")
 VISION_TOOLKIT_CYTHON_CACHE = parse_boolean_env_var("VISION_TOOLKIT_CYTHON_CACHE")
 
-SRC_DIR = pathlib.Path("src")
-
-def get_cython_pkgs_and_ext_modules():
-    ext_modules = []
-    pkgs = []
-
-    define_macros = [
-        ("CYTHON_TRACE_NOGIL", "1"),
-        # ("CYTHON_TRACE", "1"), # the macro above implies this one as well
-        # ---
-        # As of 2026, coverage has a "sysmon" core (for sys.monitoring)
-        # BUT Plugin filetracers are not supported with SysMonitoring.
-        # Since Cython default to sys.monitoring with 3.13+,
-        # we explicitly have to set it to use the old sys.settrace.
-        ("CYTHON_USE_SYS_MONITORING", "0"),
-    ] if VISION_TOOLKIT_COVERAGE else []
-
-    for p in SRC_DIR.glob("**/*.pyx"):
-        pkg_parts = p.with_suffix("").parts
-        pkgs.append(".".join(pkg_parts[1:-1]))
-        ext_modules.append(
-            Extension(
-                ".".join(pkg_parts[1:]),
-                sources=[p],
-                define_macros=define_macros,
-            )
-        )
-
-    return pkgs, ext_modules
-
-_, cython_ext_modules = get_cython_pkgs_and_ext_modules()
-
 setup(
     ext_modules= cythonize(
-        Extension("*", ["src/**/*.pyx"], define_macros=[]),
+        Extension(
+            "*",
+            ["src/**/*.pyx"],
+            define_macros=[
+                ("CYTHON_TRACE_NOGIL", "1"),
+                # ("CYTHON_TRACE", "1"), # the macro above implies this one as well
+                # ---
+                # As of 2026, coverage has a "sysmon" core (for sys.monitoring)
+                # BUT Plugin filetracers are not supported with SysMonitoring.
+                # Since Cython default to sys.monitoring with 3.13+,
+                # we explicitly have to set it to use the old sys.settrace.
+                ("CYTHON_USE_SYS_MONITORING", "0"),
+            ] if VISION_TOOLKIT_COVERAGE else [],
+        ),
         language_level=3,
         compiler_directives={"linetrace": True} if VISION_TOOLKIT_COVERAGE else {},
         cache=VISION_TOOLKIT_CYTHON_CACHE,
