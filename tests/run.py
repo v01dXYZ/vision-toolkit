9fd06b15756cd0be8bb757d9c5523a5a310a9741
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-11 11:55:43 +0100

    Add support for config args for tests/run.py

--- tests/run.py
+++ tests/run.py
@@ -1,7 +1,10 @@
 #!/bin/env python
+import sys
 import argparse
 import importlib
 import pathlib
+from vision_toolkit2.config import Config
+
 
 def get_cli_args():
     arg_parser = argparse.ArgumentParser()
@@ -10,6 +13,7 @@ def get_cli_args():
     arg_parser.add_argument("test_name", choices=["hollywood2", "zemblys"])
     arg_parser.add_argument("cutoff", type=int)
     arg_parser.add_argument("report_name", nargs="?", default="report")
+    arg_parser.add_argument("-c", "--config", help="config to add. example: distance_type=euclidean sample_frequency=1000", nargs="*", default=[])
 
     default_directory_root = "results"
     arg_parser.add_argument("-d", "--directory", help=f"{default_directory_root}/<test_name>/<version>", type=pathlib.Path)
@@ -21,9 +25,51 @@ def get_cli_args():
 
     return args
 
+def config_constructor(c):
+    r = c.split("=")
+
+    if len(r) != 2:
+        raise ValueError(f"{c!r} contains more than one '='")
+
+    config_key, config_value_str = r
+
+    dataclass_field = Config.__dataclass_fields__.get(config_key)
+
+    if dataclass_field is None:
+        raise ValueError(f"{c!r} referencing a non-existing config key")
+
+    dataclass_type = dataclass_field.type
+
+    if dataclass_type is None:
+        raise ValueError(f"{c!r} referencing a not-typed yet config key")
+
+    try:
+        config_value = dataclass_type(config_value_str)
+    except Exception:
+        raise ValueError(f"{c!r} caused an error when parsing the string of the value")
+
+    return (config_key, config_value)
 
 if __name__ == "__main__":
     args = get_cli_args()
+
+    errors = []
+    config = {}
+    for c in args.config:
+        try:
+            k, v = config_constructor(c)
+        except ValueError as ve:
+            (msg,) = ve.args
+            errors.append(msg)
+        else:
+            config[k] = v
+
+    if errors:
+        print("Error with provided config arguments", file=sys.stderr)
+        for err in errors:
+            print(" "*5, "-", err, file=sys.stderr)
+
+        exit(-1)
 
     test_mod = importlib.import_module(f"{args.test_name}")
 
@@ -32,4 +78,5 @@ if __name__ == "__main__":
         report_name=args.report_name,
         directory=args.directory,
         version=args.version,
+        config=config,
     )
