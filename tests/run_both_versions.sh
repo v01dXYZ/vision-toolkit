8c0d21e185b8315940853916e670fdce53734c02
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-24 19:22:01 +0100

    Fix test script: correct variable iteration and improve result comparison

--- tests/run_both_versions.sh
+++ tests/run_both_versions.sh
@@ -2,7 +2,7 @@
 
 test_names="hollywood2 zemblys"
 
-for test_name in test_names; do
+for test_name in $test_names; do
     for version in 1 2; do
 	$(dirname $0)/run.py --predictions $version $test_name 10000 &
     done
@@ -10,5 +10,6 @@ done
 
 wait
 
-git diff --no-index ./results/$test_name/v{1,2}/report.md
-git diff --no-index ./results/$test_name/v{1,2}/predictions
+for test_name in $test_names; do
+    git diff --no-index ./results/$test_name/v{1,2}
+done
