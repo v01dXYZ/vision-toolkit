cf7e442918e86c5d060e8ff0347ca3057bbbddf0
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-02-27 10:01:58 +0100

    WIP: tagged disjoint union, put the merging strategy as attr, remove IHOV
    the attr for merging strategy is not yet done. maybe it won't be
    implemented as it would become useless.

--- src/vision_toolkit2/config.py
+++ src/vision_toolkit2/config.py
@@ -39,104 +39,118 @@ def prefix(prefix="", class_name=False, lower=True):
     return f
 
 
-@prefix(class_name=True, lower=False)
 class HMM:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     init_low_velocity: float
     init_high_velocity: float
     init_variance: float
     nb_iters: int
 
 
-@prefix(class_name=True, lower=False)
 class I2MC:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     merging_distance_threshold: float  # [0, 1]?
     merging_duration_threshold: float  # [0, 1]?
     moving_threshold: float  # [0, 1]?
     window_duration: float
 
 
-@prefix(class_name=True, lower=False)
 class CDBA:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     initial_random_state: int
     initialization_length: str  # min | max
     maximum_iterations: int
 
 
-@prefix(class_name=True, lower=False)
 class IAP:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     centers: str  # mean | raw_IAP
 
 
-@prefix(class_name=True, lower=False)
 class IDP:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     centers: str  # mean | raw_IDP
     gaussian_kernel_sd: float
 
 
-@prefix(class_name=True, lower=False)
 class IDT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     density_threshold: float
     min_samples: int
 
 
-@prefix(class_name=True, lower=False)
 class IKM:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     cluster_number: str  # search
     min_clusters: int
     max_clusters: int
 
 
-@prefix(class_name=True, lower=False)
 class IMS:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     bandwidth: float
 
 
-@prefix(class_name=True, lower=False)
 class IDeT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     density_threshold: float
     duration_threshold: float
     min_pts: int
 
 
-@prefix(class_name=False)
 class String_Distance:
+    __config_merge__ = prefix(class_name=False)
+
     deletion_cost: float
     insertion_cost: float
     normalization: str  # min | max
 
 
-@prefix(class_name=True)
 class Levenshtein_Distance(String_Distance):
+    __config_merge__ = prefix(class_name=True)
+
     substitution_cost: float
 
 
-@prefix(class_name=True)
 class Generalized_Edit_Distance(String_Distance):
+    __config_merge__ = prefix(class_name=True)
+
     pass
 
 
-@prefix(class_name=True)
 class Needleman_Wunsch_Distance:
+    __config_merge__ = prefix(class_name=True)
+
     concordance_bonus: float
     gap_cost: 0.25
     normalization: str  # min | max
 
 
-@prefix(class_name=True)
 class Smith_Waterman:
+    __config_merge__ = prefix(class_name=True)
+
     base_deletion_cost: float
     iterative_deletion_cost: float
     similarity_threshold: float
     similarity_weight: float
 
 
-@prefix()
 class Temporal_Binning:
+    __config_merge__ = prefix()
+
     temporal_binning: bool
     temporal_binning_length: float
 
 
-@prefix(class_name=True, lower=False)
 class AoI(
     CDBA,
     IAP,
@@ -150,6 +164,8 @@ class AoI(
     Smith_Waterman,
     Temporal_Binning,
 ):
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     SPAM_support: float
     coordinates: None  # array[2, 2]
 
@@ -162,8 +178,9 @@ class AoI(
     trend_analysis_tolerance_level: None
 
 
-@prefix(class_name=True, lower=False)
 class IBDT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     duration_threshold: float
     fixation_sd: float
     fixation_threshold: float
@@ -172,22 +189,25 @@ class IBDT:
     saccade_threshold: float
 
 
-@prefix(class_name=True, lower=False)
 class ICNN:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     batch_size: int
     learning_rate: float
     num_epochs: int
     temporal_window_size: int
 
 
-@prefix(class_name=True, lower=False)
 class I_DiT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     dispersion_threshold: float
     window_duration: float
 
 
-@prefix(class_name=True, lower=False)
 class IFC:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     bcea_prob: float
     classifier: str  # to specify more closely
     i2mc: bool
@@ -195,16 +215,9 @@ class IFC:
     i2mc_window_duration: float
 
 
-@prefix(class_name=True, lower=False)
-class IHOV:
-    angular_bin_nbr: int
-    averaging_threshold: float
-    classifier: str  # to specify more closely
-    duration_threshold: float
-
-
-@prefix(class_name=True, lower=False)
 class IKF:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     chi2_threshold: float
     chi2_window: float
     chi2_sigma: float
@@ -212,50 +225,57 @@ class IKF:
     sigma_2: float
 
 
-@prefix(class_name=True, lower=False)
 class IMST:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     distance_threshold: float
     window_duration: float
     step_samples: int | None
     min_cluster_size: int | None
 
 
-@prefix(class_name=True, lower=False)
 class IVDT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     dispersion_threshold: float
     saccade_threshold: float
     window_duration: float
 
 
-@prefix(class_name=True, lower=False)
 class IVMP:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     # Paper link: Part2 l.345
     rayleigh_threshold: float
     saccade_threshold: float
     window_duration: float
 
 
-@prefix(class_name=True, lower=False)
 class IVT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     # Paper link: Part2 l.189
     velocity_threshold: float
 
 
-@prefix(class_name=True, lower=False)
 class IVVT:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     pursuit_threshold: float
     saccade_threshold: float
 
 
-@prefix(class_name=True, lower=False)
 class TDE_distance:
+    __config_merge__ = prefix(class_name=True, lower=False)
+
     method: None
     scaling: None
     subsequence_length: None
 
 
-@prefix(class_name=True)
 class Display:
+    __config_merge__ = prefix(class_name=True)
+
     _display: None  # means display itself
 
     AoI: bool
@@ -268,34 +288,38 @@ class Display:
     segmentation_path: None | str
 
 
-@prefix(class_name=True)
 class Multimatch_Simplification:
+    __config_merge__ = prefix(class_name=True)
+
     amplitude_threshold: float
     angular_threshold: float
     duration_threshold: float
     iterations: int
 
 
-@prefix(class_name=True)
 class Persistence:
+    __config_merge__ = prefix(class_name=True)
+
     display: bool
     landscape_order: int
 
 
-@prefix(class_name=True)
 class Scanmatch_Score:
+    __config_merge__ = prefix(class_name=True)
+
     concordance_bonus: None
     gap_cost: None
     substitution_threshold: None
 
 
-@prefix(class_name=True)
 class Scanpath(
     Levenshtein_Distance,
     Needleman_Wunsch_Distance,
     Generalized_Edit_Distance,
     Temporal_Binning,
 ):
+    __config_merge__ = prefix(class_name=True)
+
     CRQA_distance_threshold: float
     CRQA_minimum_length: float
     RQA_distance_threshold: float
@@ -305,25 +329,37 @@ class Scanpath(
     spatial_binning_nb_pixels_y: int
 
 
-@prefix()
-class Smoothing:
-    smoothing: str  # moving_average | speed_moving_average | savgol
+# class Smoothing:
+#     __config_merge__ = prefix()
+
+#     smoothing: str  # moving_average | speed_moving_average | savgol
+
+Smoothing = tagged_union_disjoint_types(
+    "Smoothing",
+    "smoothing",
+    {
+        "savgol": Savgol,
+    }
+)
 
 
-@prefix(class_name=True)
 class Savgol:
+    __config_merge__ = prefix(class_name=True)
+
     polyorder: int
     window_length: int
 
 
-@prefix(class_name=True)
 class Fixation:
+    __config_merge__ = prefix(class_name=True)
+
     weighted_average_velocity_means: bool
     BCEA_probability: float
 
 
-@prefix(class_name=True)
 class Saccade:
+    __config_merge__ = prefix(class_name=True)
+
     absolute_horizontal_deviations: bool
     init_direction_duration_threshold: float
     init_deviation_duration_threshold: float
@@ -333,8 +369,9 @@ class Saccade:
     weighted_average_deceleration_means: bool
 
 
-@prefix(class_name=True)
 class Pursuit:
+    __config_merge__ = prefix(class_name=True)
+
     end_idx: None | int
 
     onset_baseline_length: None
@@ -353,7 +390,6 @@ class Common(
     ICNN,
     I_DiT,
     IFC,
-    IHOV,
     IKF,
     IMST,
     IVDT,
@@ -481,3 +517,76 @@ class StackedConfig(ConfigMixin):
                 return attr
 
         return None
+
+
+def tagged_union_disjoint_types(class_name: str, tag_name: str, classes: dict[str, type]) -> type:
+    """
+    Creates a tagged union dataclass with __disjoint_types__ attribute.
+
+    Args:
+        class_name: Name of the generated dataclass
+        tag_name: Name of the tag attribute
+        classes: Dictionary mapping tag values to dataclass types
+
+    Returns:
+        A dataclass with __disjoint_types__ attribute and smart initialization
+    """
+    # Convert classes dict to list of (tag_value, type) tuples
+    disjoint_types = list(classes.items())
+
+    def __init__(self, variant):
+        # Determine the tag based on the variant's type
+        variant_class = type(variant)
+
+        # Build reverse mapping from class to tag using self.__disjoint_types__
+        class_to_tag = {
+            cls: tag
+            for tag, cls in self.__disjoint_types__
+        }
+
+        if variant_class not in class_to_tag:
+            raise TypeError(
+                f"Unsupported variant type: {variant_class}. "
+                f"Supported types: {[cls.__name__ for cls in class_to_tag.keys()]}"
+            )
+
+        tag_value = class_to_tag[variant_class]
+
+        # Set the tag using self.__tag__
+        setattr(self, self.__tag__, tag_value)
+
+        # Set the variant in the correct attribute
+        setattr(self, tag_value, variant)
+
+    # Create the dataclass using make_dataclass
+    cls = dataclasses.make_dataclass(
+        class_name,
+        [(tag_name, str)] + [cls for _, cls in disjoint_types],
+        namespace={
+            '__tag__': tag_name,
+            '__disjoint_types__': disjoint_types,
+            '__init__': __init__
+        }
+    )
+
+    return cls
+
+Segmentation = tagged_union_disjoint_types(
+    "Segmentation",
+    "segmentation_method",
+    {
+        "HMM": HMM,
+        "I2MC": I2MC,
+        "IBDT": IBDT,
+        "ICNN": ICNN,
+        "I_DiT": I_DiT,
+        "IFC": IFC,
+        "IKF": IKF,
+        "IMST": IMST,
+        "IVDT": IVDT,
+        "IVMP": IVMP,
+        "IVT": IVT,
+        "IVVT": IVVT,
+        "IDeT": IDeT,
+    }
+)
