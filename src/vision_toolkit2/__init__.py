0a6016d30c1942bd2612bcd1a3c544e63da90e32
Robert Dazi <14996868+v01dXYZ@users.noreply.github.com>
2026-03-03 09:38:11 +0100

    Add function to easily query the example dataset directory

--- src/vision_toolkit2/__init__.py
+++ src/vision_toolkit2/__init__.py
@@ -2,6 +2,9 @@ from .segmentation.base_segmentation import Segmentation
 from .config import Config, StackedConfig
 from .serie import Serie
 
+def example_dataset_dir():
+    return f'{__file__.replace("__init__.py", "")}/example_dataset'
+
 __all__ = [
     Segmentation,
     Config,
